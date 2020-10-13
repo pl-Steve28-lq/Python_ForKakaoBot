80acc7bf23d53a187eb65538bb4fc2e1d6c123eb
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-10-13 23:22:01 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import json
+from script import response
+from replierClass import replier, KakaoLink
+
+app = Flask('app')
+
+@app.route('/<info>')
+def resp(info):
+  data = json.loads(info.replace('\'', '\"').replace('=pl_Steve28_lq=', '/'))
+  room = data['room']
+  msg = data['msg']
+  sender = data['sender']
+  isGroupChat = data['isGroupChat']
+  packageName = data['packageName']
+  isDebugChat = data['isDebugChat']
+  
+  r = replier(packageName, room, isDebugChat)
+  r.clear()
+  k = KakaoLink()
+  return str(response(room, msg, sender, isGroupChat, r, packageName, k))
+
+app.run(host='127.0.0.1', port=8080)
