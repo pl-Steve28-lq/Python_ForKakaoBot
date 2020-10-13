3a07b89f1a869fccf0ec84eec4c956bd2b17425a
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-10-13 23:40:17 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 def response(room, msg, sender, isGroupChat, replier, packageName, Kakao):
   if msg == "안녕!":
     replier.reply("안녕하세요.")
-  
+    Kakao.send(room, 대충 템플릿 JSON 이라는 것, "custom")
+  If sender == "Steve28":
+    replier.reply("안녕하세요, " + sender + "님!")
+
   return replier.data
